c2bd7086e78657389c5b709c7a6a733de795f9d1
UmaisRehman <umaisrehman109@gmail.com.com>
2024-09-28 13:07:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import './App.css'; 
+
+function App() {
+  let [num, setNum] = useState(0);
+
+  function addCounter() {
+    setNum(num + 1);
+  }
+  function MinCounter() {
+    setNum(num - 1);
+  }
+
+  return (
+    <>
+      <h1>Count {num}</h1>
+      <button onClick={addCounter}>+ Plus Counter </button>
+      <button onClick={MinCounter}>- Minus Counter </button>
+    </>
+  );
+}
+
+export default App;
